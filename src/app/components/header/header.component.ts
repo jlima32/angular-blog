a4bd3c03160fc69077b26d16ff479faa0eeb7212
Juan Lima <juangustavo@gmail.com>
2025-02-18 08:09:50 -0300

    nav-bar component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../logo/logo.component";
 import { SearchComponent } from "../search/search.component";
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [LogoComponent, SearchComponent],
+  imports: [LogoComponent, SearchComponent, NavBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
