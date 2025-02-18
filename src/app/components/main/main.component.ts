b4d2fd7adbeea3f3fd95511dbf40f189e90ce39b
Juan Lima <juangustavo@gmail.com>
2025-02-18 09:59:11 -0300

    post component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { BigCardComponent } from "../big-card/big-card.component";
 import { SmallCardComponent } from "../small-card/small-card.component";
+import { TitleComponent } from "../title/title.component";
+import { PostComponent } from "../post/post.component";
 
 @Component({
   selector: 'app-main',
-  imports: [SidebarComponent, BigCardComponent, SmallCardComponent],
+  imports: [SidebarComponent, BigCardComponent, SmallCardComponent, TitleComponent, PostComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NewsletterComponent } from "../newsletter/newsletter.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NewsletterComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
