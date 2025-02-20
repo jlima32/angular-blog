add74b4b9d558a4eb9d4587ae601ec43a691330a
Juan Lima <juangustavo@gmail.com>
2025-02-20 15:33:28 -0300

    criando outras páginas

---
+++ src/app/data/data.ts
@@ -0,0 +1,35 @@
+export const data = [
+  {
+    "id": 1,
+    "cover": "https://cdn.pixabay.com/photo/2015/09/04/23/28/wordpress-923188_1280.jpg",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate!",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate! Recusandae tempore tenetur dignissimos minima neque impedit sunt nesciunt itaque debitis maxime laboriosam vel, corporis unde magnam pariatur architecto repudiandae!",
+    "user": "Admin",
+    "date": "20 Jan 2025",
+    "day": 20,
+    "comments": 12,
+    "category": "Tecnologia"
+  },
+  {
+    "id": 2,
+    "cover": "https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate!",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate! Recusandae tempore tenetur dignissimos minima neque impedit sunt nesciunt itaque debitis maxime laboriosam vel, corporis unde magnam pariatur architecto repudiandae!",
+    "user": "Admin",
+    "date": "18 Jan 2025",
+    "day": 18,
+    "comments": 8,
+    "category": "Tecnologia"
+  },
+  {
+    "id": 3,
+    "cover": "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate!",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptate! Recusandae tempore tenetur dignissimos minima neque impedit sunt nesciunt itaque debitis maxime laboriosam vel, corporis unde magnam pariatur architecto repudiandae!",
+    "user": "Admin",
+    "date": "15 Jan 2025",
+    "day": 15,
+    "comments": 2,
+    "category": "Tecnologia"
+  }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { TitleComponent } from "../../components/title/title.component";
+import { Component, Input, OnInit } from '@angular/core';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-content',
-  imports: [TitleComponent, SidebarComponent],
+  imports: [SidebarComponent],
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css', './content.responsive.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+
+    @Input()
+    postTitle:string = "";
+    @Input()
+    postText:string = "";
+    @Input()
+    postUser:string = "";
+    @Input()
+    postDate:string = "";
+    @Input()
+    postDay:string = "";
+    @Input()
+    postComments:string = "";
+    @Input()
+    postCategory:string = "";
+
+  ngOnInit(): void {
+  }
 
 }
