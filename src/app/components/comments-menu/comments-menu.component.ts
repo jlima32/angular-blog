65875825d63bc6adcec70feed381634b9032545b
Juan Lima <juangustavo@gmail.com>
2025-02-20 09:16:40 -0300

    comments-menu component

---
+++ src/app/components/comments-menu/comments-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-menu',
+  imports: [],
+  templateUrl: './comments-menu.component.html',
+  styleUrl: './comments-menu.component.css'
+})
+export class CommentsMenuComponent {
+
+}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NewsletterComponent } from "../newsletter/newsletter.component";
 import { CategoryMenuComponent } from "../category-menu/category-menu.component";
+import { CommentsMenuComponent } from "../comments-menu/comments-menu.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NewsletterComponent, CategoryMenuComponent],
+  imports: [NewsletterComponent, CategoryMenuComponent, CommentsMenuComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
