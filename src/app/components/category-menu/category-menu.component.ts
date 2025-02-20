2f25120da108ccc3dcf4e1ad041a1ce95df5c383
Juan Lima <juangustavo@gmail.com>
2025-02-20 09:10:42 -0300

    category-menu component

---
+++ src/app/components/category-menu/category-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-menu',
+  imports: [],
+  templateUrl: './category-menu.component.html',
+  styleUrl: './category-menu.component.css'
+})
+export class CategoryMenuComponent {
+
+}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NewsletterComponent } from "../newsletter/newsletter.component";
+import { CategoryMenuComponent } from "../category-menu/category-menu.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NewsletterComponent],
+  imports: [NewsletterComponent, CategoryMenuComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
